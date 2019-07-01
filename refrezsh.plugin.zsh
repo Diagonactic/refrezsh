6f5fc79a35da3d0275f6708b0f24f76694a83ac8
Matthew S. Dippel <mdippel@vectorform.com>
2019-06-30 20:45:10 -0400

    Update refrezsh.plugin.zsh

--- refrezsh.plugin.zsh
+++ refrezsh.plugin.zsh
@@ -39,11 +39,11 @@ function __refrezsh-prompt() {
     (( EUID == 0 )) && local PARROW="${refrezsh_icons[root-icon-fg]}${refrezsh_icons[root-icon]}" || local PARROW="${refrezsh_icons[user-icon-fg]}${refrezsh_icons[user-icon]}"
 
     if [[ -n "${SSH_CONNECTION}" ]]; then
-        local SSH_ICON=$'%{\e[38;2;240;0;25m%}'"${refrezsh_icons[ssh-icon]} ${SSH_CONNECTION%% *}:"$'%{\e[1m}'"$HOST"
+        local SSH_ICON=$'%{\e[38;2;240;0;25m%}'"${refrezsh_icons[ssh-icon]} ${SSH_CONNECTION%% *}:"$'%{\e[1m%}'"$HOST"
         PARROW="$SSH_ICON "$'%{\e[0;37m%}'"$PARROW"
     fi
     setopt promptsubst
-    PROMPT=$'\n'"$PATH_GROUP$ACCOUNT_GROUP$VCS_GROUP$END_GROUP ${refrezsh[prompt-end]}"$'\n%{\e[38;2;255;0;0m%}'"%(?:: %? )"$'%{\e[0;37;40m%}'"$PARROW"$'%{\e[0;37;40m} '
+    PROMPT=$'\n'"$PATH_GROUP$ACCOUNT_GROUP$VCS_GROUP$END_GROUP ${refrezsh[prompt-end]}"$'\n%{\e[38;2;255;0;0m%}'"%(?:: %? )"$'%{\e[0;37;40m%}'"$PARROW"$'%{\e[0;37;40m%} '
     RPROMPT=''
     debug_logs() >> ~/tmp/last_prompt.info {
         dtitle() {
